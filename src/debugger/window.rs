0ba918a622c514a99cbac3487b7d375b610ae70b
ba-lindner <147381263+ba-lindner@users.noreply.github.com>
2025-04-12 22:56:06 +0200

    use separate thread for sdlwindow

--- src/debugger/window.rs
+++ src/debugger/window.rs
@@ -1,3 +1,5 @@
+use std::{sync::mpsc::{self, Receiver, Sender}, thread, time::Duration};
+
 use sdl2::{event::Event, pixels::Color, rect::Point, render::Canvas, EventPump};
 
 use super::{TColor, TCoord};
@@ -69,17 +71,24 @@ pub enum WindowEvent {
     MouseClicked(TCoord, bool),
 }
 
-pub struct SdlWindow {
-    state: SdlState,
-    max_coord: (f64, f64),
+enum WindowCmd {
+    Draw(Color, Point, Point),
+    Clear,
 }
 
-enum SdlState {
-    Uninit(String),
-    Init {
-        canvas: Canvas<sdl2::video::Window>,
-        event_pump: EventPump,
-    },
+pub struct SdlWindow {
+    title: String,
+    max_coord: (f64, f64),
+    cmds: Sender<WindowCmd>,
+    events: Receiver<WindowEvent>,
+}
+
+struct SdlImpl {
+    canvas: Canvas<sdl2::video::Window>,
+    event_pump: EventPump,
+    cmds: Receiver<WindowCmd>,
+    events: Sender<WindowEvent>,
+    wait_exit: Option<usize>
 }
 
 const WIDTH: u32 = 800;
@@ -88,8 +97,10 @@ const HEIGHT: u32 = 600;
 impl SdlWindow {
     pub fn new(title: &str) -> Self {
         Self {
-            state: SdlState::Uninit(title.to_string()),
+            title: title.to_string(),
             max_coord: (0.0, 0.0),
+            cmds: mpsc::channel().0,
+            events: mpsc::channel().1,
         }
     }
 
@@ -101,62 +112,58 @@ impl SdlWindow {
     }
 }
 
-impl Window for SdlWindow {
-    fn init(&mut self, max_x: f64, max_y: f64) {
-        self.max_coord = (max_x, max_y);
-        let SdlState::Uninit(title) = &self.state else {
-            panic!("already initialized");
-        };
-        let sdl_context = sdl2::init().unwrap();
-        let video_sub = sdl_context.video().unwrap();
-        let window = video_sub
-            .window(title, WIDTH, HEIGHT)
-            .position_centered()
-            .build()
-            .unwrap();
-        let canvas = window.into_canvas().build().unwrap();
-        let event_pump = sdl_context.event_pump().unwrap();
-        self.state = SdlState::Init { canvas, event_pump };
-        self.clear();
+impl SdlImpl {
+    fn spawn(title: String, cmds: Receiver<WindowCmd>, events: Sender<WindowEvent>) {
+        thread::spawn(move || {
+            let sdl_context = sdl2::init().unwrap();
+            let video_sub = sdl_context.video().unwrap();
+            let window = video_sub
+                .window(&title, WIDTH, HEIGHT)
+                .position_centered()
+                .build()
+                .unwrap();
+            let mut canvas = window.into_canvas().build().unwrap();
+            canvas.clear();
+            let event_pump = sdl_context.event_pump().unwrap();
+            let this = SdlImpl {
+                canvas,
+                event_pump,
+                cmds,
+                events,
+                wait_exit: None,
+            };
+            this.run();
+        });
     }
 
-    fn max_coords(&mut self) -> &mut (f64, f64) {
-        &mut self.max_coord
+    fn run(mut self) {
+        loop {
+            for cmd in self.cmds.try_iter() {
+                match cmd {
+                    WindowCmd::Clear => {
+                        self.canvas.set_draw_color(Color::BLACK);
+                        self.canvas.clear();
+                    }
+                    WindowCmd::Draw(color, start, end) => {
+                        self.canvas.set_draw_color(color);
+                        self.canvas.draw_line(start, end).unwrap();
+                    }
+                }
+            }
+            self.canvas.present();
+            thread::sleep(Duration::from_millis(16));
+            if let Some(rem) = &mut self.wait_exit {
+                *rem -= 1;
+                if *rem == 0 {
+                    std::process::exit(0);
+                }
+            }
+            self.collect_events();
+        }
     }
 
-    fn draw(&mut self, from: TCoord, to: TCoord, col: TColor) {
-        let start = self.map_coords(from);
-        let end = self.map_coords(to);
-        let SdlState::Init { canvas, .. } = &mut self.state else {
-            panic!("uninitialized");
-        };
-        canvas.set_draw_color(Color::RGB(
-            (col.0 * 2.55) as u8,
-            (col.1 * 2.55) as u8,
-            (col.2 * 2.55) as u8,
-        ));
-        canvas.draw_line(start, end).unwrap();
-        canvas.present();
-    }
-
-    fn clear(&mut self) {
-        let SdlState::Init { canvas, .. } = &mut self.state else {
-            panic!("uninitialized");
-        };
-        canvas.set_draw_color(Color::BLACK);
-        canvas.clear();
-        canvas.present();
-    }
-
-    fn print(&mut self, msg: &str) {
-        println!("Turtle says: {msg}");
-    }
-
-    fn events(&mut self) -> Vec<WindowEvent> {
-        let SdlState::Init { event_pump, .. } = &mut self.state else {
-            panic!("uninitialized");
-        };
-        event_pump
+    fn collect_events(&mut self) {
+        let events = self.event_pump
             .poll_iter()
             .flat_map(|evt| match evt {
                 Event::KeyDown {
@@ -167,20 +174,78 @@ impl Window for SdlWindow {
                     .ok()
                     .and_then(char::from_u32)
                     .map(WindowEvent::KeyPressed),
-                Event::Quit { .. } => Some(WindowEvent::WindowExited),
+                Event::Quit { .. } => {
+                    self.wait_exit.get_or_insert(5);
+                    Some(WindowEvent::WindowExited)
+                }
                 Event::MouseButtonDown {
                     x, y, mouse_btn, ..
                 } => {
-                    let x = (x as f64 * 2.0 / WIDTH as f64 - 1.0) * self.max_coord.0;
-                    let y = (1.0 - y as f64 * 2.0 / HEIGHT as f64) * self.max_coord.1;
                     Some(WindowEvent::MouseClicked(
-                        (x, y),
+                        (x as f64, y as f64),
                         mouse_btn == sdl2::mouse::MouseButton::Left,
                     ))
                 }
                 _ => None,
-            })
-            .collect()
+            });
+        for evt in events {
+            self.events.send(evt).unwrap();
+        }
+    }
+}
+
+impl Window for SdlWindow {
+    fn init(&mut self, max_x: f64, max_y: f64) {
+        self.max_coord = (max_x, max_y);
+        let (cmd_tx, cmd_rx) = mpsc::channel();
+        let (evt_tx, evt_rx) = mpsc::channel();
+        let title = std::mem::take(&mut self.title);
+        SdlImpl::spawn(title, cmd_rx, evt_tx);
+        self.cmds = cmd_tx;
+        self.events = evt_rx;
+    }
+
+    fn max_coords(&mut self) -> &mut (f64, f64) {
+        &mut self.max_coord
+    }
+
+    fn draw(&mut self, from: TCoord, to: TCoord, col: TColor) {
+        let col = Color::RGB(
+            (col.0 * 2.55) as u8,
+            (col.1 * 2.55) as u8,
+            (col.2 * 2.55) as u8,
+        );
+        let start = self.map_coords(from);
+        let end = self.map_coords(to);
+        self.cmds.send(WindowCmd::Draw(col, start, end)).unwrap();
+        // canvas.set_draw_color(Color::RGB(
+        //     (col.0 * 2.55) as u8,
+        //     (col.1 * 2.55) as u8,
+        //     (col.2 * 2.55) as u8,
+        // ));
+        // canvas.draw_line(start, end).unwrap();
+        // canvas.present();
+    }
+
+    fn clear(&mut self) {
+        self.cmds.send(WindowCmd::Clear).unwrap();
+        // canvas.set_draw_color(Color::BLACK);
+        // canvas.clear();
+        // canvas.present();
+    }
+
+    fn print(&mut self, msg: &str) {
+        println!("Turtle says: {msg}");
+    }
+
+    fn events(&mut self) -> Vec<WindowEvent> {
+        self.events.try_iter().map(|mut e| {
+            if let WindowEvent::MouseClicked((x, y), _) = &mut e {
+                *x = (*x as f64 * 2.0 / WIDTH as f64 - 1.0) * self.max_coord.0;
+                *y = (1.0 - *y as f64 * 2.0 / HEIGHT as f64) * self.max_coord.1;
+            }
+            e
+        }).collect()
     }
 }
 
