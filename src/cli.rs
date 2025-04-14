c4e3f5909113bd010cdf3cfedd8a18f4b6229593
ba-lindner <147381263+ba-lindner@users.noreply.github.com>
2025-04-14 21:53:27 +0200

    allow base program for shell

---
+++ src/cli.rs
@@ -0,0 +1,118 @@
+use clap::{Args, Parser, Subcommand, ValueEnum};
+use turtle::{features::Feature, pos::FilePos};
+
+#[derive(Parser)]
+#[command(version)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: TCommand,
+}
+
+#[derive(Subcommand)]
+pub enum TCommand {
+    /// Start interpreter
+    Run {
+        #[command(flatten)]
+        source: Source,
+        /// const-fold before execution
+        #[arg(short, long)]
+        optimized: bool,
+        #[command(flatten)]
+        opt: RunOpt,
+    },
+    /// Start turtle shell
+    Shell {
+        /// source file to load functions from
+        #[arg(short, long)]
+        base: Option<String>,
+        #[command(flatten)]
+        features: Features,
+    },
+    /// Start debugger
+    Debug {
+        #[command(flatten)]
+        source: Source,
+        /// set breakpoints in format line,column
+        #[arg(short, long)]
+        breakpoint: Vec<FilePos>,
+        /// select debugging interface
+        #[arg(short, long, default_value = "terminal")]
+        interface: Interf,
+        #[command(flatten)]
+        opt: RunOpt,
+    },
+    /// Compile to C
+    Compile {
+        #[command(flatten)]
+        source: Source,
+    },
+    /// Check syntax
+    Check {
+        #[command(flatten)]
+        source: Source,
+        /// print symbols on error
+        #[arg(short = 's', long)]
+        print_symbols: bool,
+    },
+}
+
+#[derive(Args)]
+#[group()]
+pub struct Source {
+    /// turtle source file
+    pub file: String,
+    #[command(flatten)]
+    pub features: Features,
+}
+
+#[derive(Args)]
+#[group()]
+pub struct Features {
+    /// enabled features
+    #[arg(short, long = "feature")]
+    pub features: Vec<Feature>,
+    /// disabled features
+    #[arg(short = 'F', long)]
+    pub disabled: Vec<Feature>,
+}
+
+#[derive(Args)]
+#[group()]
+pub struct RunOpt {
+    /// display mechanism
+    #[arg(short, long, default_value = "sdl")]
+    pub window: Display,
+    /// args passed to turtle
+    #[arg(last = true)]
+    pub args: Vec<String>,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug, Default)]
+pub enum Display {
+    /// use SDL2 to draw turtles
+    #[default]
+    Sdl,
+    /// no display
+    Void,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug, Default)]
+pub enum Interf {
+    /// run as standalone debugger
+    #[default]
+    Terminal,
+    /// run as vscode extension
+    VSCode,
+}
+
+#[cfg(test)]
+mod test {
+    use clap::CommandFactory;
+
+    use super::*;
+
+    #[test]
+    fn cli() {
+        Cli::command().debug_assert();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,19 @@
-use clap::{Args, Parser, Subcommand, ValueEnum};
+use clap::Parser;
 use turtle::{
     debugger::{
         config::RunConfig,
         interface::{Shell, Terminal, VSCode},
         window::{SdlWindow, Window},
     },
-    features::{Feature, FeatureConf, FeatureState},
-    pos::FilePos,
+    features::{FeatureConf, FeatureState},
     TProgram,
 };
 
-#[derive(Parser)]
-#[command(version)]
-struct Cli {
-    #[command(subcommand)]
-    command: TCommand,
-}
+use cli::*;
 
-#[derive(Subcommand)]
-enum TCommand {
-    /// Start interpreter
-    Run {
-        #[command(flatten)]
-        source: Source,
-        /// const-fold before execution
-        #[arg(short, long)]
-        optimized: bool,
-        #[command(flatten)]
-        opt: RunOpt,
-    },
-    /// Start turtle shell
-    Shell {
-        /// enabled features
-        #[arg(short, long = "feature")]
-        features: Vec<Feature>,
-        /// disabled features
-        #[arg(short = 'F', long)]
-        disabled: Vec<Feature>,
-    },
-    /// Start debugger
-    Debug {
-        #[command(flatten)]
-        source: Source,
-        /// set breakpoints in format line,column
-        #[arg(short, long)]
-        breakpoint: Vec<FilePos>,
-        /// select debugging interface
-        #[arg(short, long, default_value = "terminal")]
-        interface: Interf,
-        #[command(flatten)]
-        opt: RunOpt,
-    },
-    /// Compile to C
-    Compile {
-        #[command(flatten)]
-        source: Source,
-    },
-    /// Check syntax
-    Check {
-        #[command(flatten)]
-        source: Source,
-        /// print symbols on error
-        #[arg(short = 's', long)]
-        print_symbols: bool,
-    },
-}
+mod cli;
 
-#[derive(Args)]
-#[group()]
-struct Source {
-    /// turtle source file
-    file: String,
-    /// enabled features
-    #[arg(short, long = "feature")]
-    features: Vec<Feature>,
-    /// disabled features
-    #[arg(short = 'F', long)]
-    disabled: Vec<Feature>,
-}
-
-impl Source {
+impl Features {
     fn feature_conf(&self) -> FeatureConf {
         let mut res = FeatureConf::default();
         for f in &self.features {
@@ -90,9 +24,11 @@ impl Source {
         }
         res
     }
+}
 
+impl Source {
     fn get_prog(&self) -> TProgram {
-        match TProgram::from_file(&self.file, false, self.feature_conf()) {
+        match TProgram::from_file(&self.file, false, self.features.feature_conf()) {
             Ok(prog) => prog,
             Err(why) => {
                 eprintln!("invalid turtle program: {why}");
@@ -102,49 +38,22 @@ impl Source {
     }
 
     fn check(&self, print_symbols: bool) {
-        if let Err(why) = TProgram::from_file(&self.file, print_symbols, self.feature_conf()) {
+        if let Err(why) =
+            TProgram::from_file(&self.file, print_symbols, self.features.feature_conf())
+        {
             eprintln!("invalid turtle program: {why}");
             std::process::exit(1)
         }
     }
 }
 
-#[derive(Args)]
-#[group()]
-struct RunOpt {
-    /// display mechanism
-    #[arg(short, long, default_value = "sdl")]
-    window: Display,
-    /// args passed to turtle
-    #[arg(last = true)]
-    args: Vec<String>,
-}
-
 impl RunOpt {
     fn config<'a>(&'a self, title: &str) -> RunConfig<'a, Box<dyn Window>, Terminal> {
         RunConfig::new(&self.args).window(match self.window {
-            Display::SDL => Box::new(SdlWindow::new(title)),
+            Display::Sdl => Box::new(SdlWindow::new(title)),
             Display::Void => Box::new((20.0, 15.0)),
         })
     }
-}
-
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug, Default)]
-pub enum Display {
-    /// use SDL2 to draw turtles
-    #[default]
-    SDL,
-    /// no display
-    Void,
-}
-
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug, Default)]
-pub enum Interf {
-    /// run as standalone debugger
-    #[default]
-    Terminal,
-    /// run as vscode extension
-    VSCode,
 }
 
 fn main() {
@@ -161,19 +70,13 @@ fn main() {
             }
             opt.config(&prog.title("Interpreter")).exec(&prog);
         }
-        TCommand::Shell { features, disabled } => {
-            let features = Source {
-                file: String::new(),
-                features,
-                disabled,
-            }
-            .feature_conf();
-            match TProgram::parse("begin end".to_string(), false, features) {
-                Ok(prog) => {
-                    RunConfig::new(&[]).debug_in(Shell).exec(&prog);
-                }
-                Err(why) => eprintln!("{why}"),
-            }
+        TCommand::Shell { features, base } => {
+            let prog = if let Some(file) = base {
+                Source { features, file }.get_prog()
+            } else {
+                TProgram::parse("begin end".to_string(), false, features.feature_conf()).unwrap()
+            };
+            RunConfig::new(&[]).debug_in(Shell).exec(&prog);
         }
         TCommand::Debug {
             source,
@@ -207,16 +110,4 @@ fn compile(prog: TProgram, filename: &str) {
     );
     cc.set_filename(&resfile);
     cc.compile();
-}
-
-#[cfg(test)]
-mod test {
-    use clap::CommandFactory;
-
-    use super::*;
-
-    #[test]
-    fn cli() {
-        Cli::command().debug_assert();
-    }
 }
