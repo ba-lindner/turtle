161105afb5c10371b6507a16c120486cfdb7e574
ba-lindner <147381263+ba-lindner@users.noreply.github.com>
2025-07-05 15:18:12 +0200

    WIP website: example list

---
+++ turtle-web/assets/scripts/example_list.js
@@ -0,0 +1,42 @@
+function runExample(ex) {
+    $.ajax(`/examples/${ex}/run`, {
+        type: "POST",
+        data: JSON.stringify([]),
+        contentType: "application/json",
+        success: function (uuid) {
+            $.ajax(`/run/${uuid}/debug`, {
+                type: "POST",
+                data: JSON.stringify("run"),
+                contentType: "application/json",
+            })
+            window.location.href = `/viewRun?id=${uuid}`
+        }
+    })
+}
+
+$(function () {
+    function appendGroup(group) {
+        $("#example-list").append(`<h3>${group}</h3>`)
+    }
+
+    function appendExample(ex) {
+        let div = '<div class="example">'
+            + `<button class="run" onClick="runExample('${ex.name}')">⏵<span class="tooltip">Run Example</span></button>`
+            + `<span class="name">${ex.name}</span>`
+            + "<br>"
+            + `<span class="descr">${ex.summary}</span>`
+            + "</div>"
+        $("#example-list").append(div)
+    }
+
+    $.getJSON("/examples", function (data) {
+        let lastGroup = ""
+        for (const ex of data) {
+            if (ex.group != lastGroup) {
+                lastGroup = ex.group
+                appendGroup(ex.group)
+            }
+            appendExample(ex)
+        }
+    })
+});
